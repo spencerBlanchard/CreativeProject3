25a7d5d8aca15bc34ec4cc802dc3773067ba11ea
Sterling Smith <sterling.smith.stl@gmail.com>
2022-10-27 22:11:18 +0000

    Hack is now react

--- my-app/src/pages/Hack.js
+++ my-app/src/pages/Hack.js
@@ -1,70 +1,122 @@
-    const Hack = () => {
-    return (
-        <div>
-        <script>
-            
-            var success = false;
-        
-            function submitString() {
-                let submission = document.getElementById("stringGrabber").value;
-                let length = submission.length;
-                if (length != 6) {
-                    document.getElementById("correctSpace").innerHTML = 0;
-                    document.getElementById("wrongSpace").innerHTML = 0;
-                    return;
-                }
-                let wS = 0;
-                let cS = 0;
-                const answer = "#'cX%0";
+import React from "react"
 
-                for (let i = 0; i < 6; ++i) {
-                    if (answer.charAt(i) === submission.charAt(i)) {
-                        ++cS;
-                    }
-                    else if (submission.search(answer.charAt(i)) != -1) {
-                        ++wS;
-                    }
-                }
+class Hack extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            success: false,
+            value: "",
+            correctSpace: 0,
+            wrongSpace: 0
+        }
+        this.submitString = this.submitString.bind(this);
 
-                document.getElementById("correctSpace").innerHTML = cS;
-                document.getElementById("wrongSpace").innerHTML = wS;
-                
-                if (cS === 6 && success === false) {
-                    success = true;
-                    showSuccess();
-                }
+        this.handleChange = this.handleChange.bind(this);
+        this.navigateToWin = this.navigateToWin.bind(this);
+        // this.showSuccess = this.showSuccess.bind(this);
 
-                return;
+    }
+
+
+    submitString(event) {
+        event.preventDefault();
+        let submission = this.state.value;
+        console.log(submission);
+        let length = submission.length;
+        if (length != 6) {
+            this.state.correctSpace = 0;
+            this.state.wrongSpace = 0;
+            return;
+        }
+
+        let wS = 0;
+        let cS = 0;
+        // const answer = "#'cX%0";
+        const answer = "asdfas";
+
+        for (let i = 0; i < 6; ++i) {
+            if (answer.charAt(i) === submission.charAt(i)) {
+                ++cS;
             }
-
-            function showSuccess() {
-                let btn = document.createElement("button");
-                btn.innerHTML = "Success!!!";
-                btn.onclick = function() {
-                    alert("You clicked the success button!");
-                };
-                document.body.appendChild(btn);
-
+            else if (submission.search(answer.charAt(i)) != -1) {
+                ++wS;
             }
-        </script>
-        <form action="javascript:submitString()">
-            <label for="stringGrabber">Input a 6-character string:</label><br>
-            <input type="text" id="stringGrabber" name="fname"><br>
-            <input type="submit" id="button" value="Submit">
-        </form>
-        <table>
-            <tr>
-                <th>Number of correct characters in the correct space</th>
-                <th>Number of correct characters in the wrong space</th>
-            </tr>
-            <tr>
-                <td id="correctSpace"></td>
-                <td id="wrongSpace"></td>
-            </tr>
-        </table>
-        <div id="ifSuccessful"></div>
-    </div>
-    );
+        }
+
+        this.setState({ correctSpace: cS });
+        this.setState({ wrongSpace: wS });
+
+        if (cS === 6 && this.state.success === false) {
+            this.state.success = true;
+            // this.showSuccess();
+        }
+
+        return;
+    }
+
+    // showSuccess() {
+
+    //     // let btn = document.createElement("button");
+    //     // btn.innerHTML = "Success!!!";
+    //     // btn.onclick = function() {
+    //     //     alert("You clicked the success button!");
+    //     // };
+    //     // document.body.appendChild(btn);
+    //     return;
+    // }
+
+    navigateToWin () {
+        console.log("go to win page");
+        // navigate("Win.js");
+    }
+
+    handleChange(event) {
+        // console.log("you are handling a change");
+
+        this.setState({ value: event.target.value });
+
+        this.setState((prevState) => ({ value: event.target.value }))
+    }
+
+    render() {
+        let button;
+        if (this.state.success) {
+            button = <button onClick={this.navigateToWin}>ButtonName</button>
+        }
+        else {
+            button = <div/>
+        }
+
+        if (this.state.success) {
+
+        }
+
+        return (
+            <div>
+            <form onSubmit={this.submitString}>
+                <label>Input a 6-character string:
+                    <input type="text" onChange={this.handleChange} value={this.state.value}/><br/>
+                </label>
+                <input type="submit" id="button" value="Submit"/>
+            </form>
+            <table>
+                <tr>
+                    <th>Number of correct characters in the correct space</th>
+                    <th>Number of correct characters in the wrong space</th>
+                </tr>
+                <tr>
+                    <td>{this.state.correctSpace}</td>
+                    <td>{this.state.wrongSpace}</td>
+                </tr>
+                {button}
+            </table>
+        </div>
+        );
+    }
+
 }
+
+
+
 
 export default Hack;
