b84b08f14d02ce688f8e27db963920f687d50591
Sterling Smith <sterling.smith.stl@gmail.com>
2022-10-28 18:39:42 +0000

    added a message for the TAs

--- my-app/src/pages/Hack.js
+++ my-app/src/pages/Hack.js
@@ -12,10 +12,10 @@ class Hack extends React.Component {
             wrongSpace: 0
         }
         this.submitString = this.submitString.bind(this);
-
         this.handleChange = this.handleChange.bind(this);
         this.navigateToWin = this.navigateToWin.bind(this);
 
+        this.TAsHint = this.TAsHint.bind(this);
     }
 
 
@@ -34,8 +34,8 @@ class Hack extends React.Component {
 
         let wS = 0;
         let cS = 0;
-        // const answer = "#'cX%0";
-        const answer = "asdfas";
+        const answer = "#'cX%0";
+        // const answer = "asdfas";
 
         for (let i = 0; i < 6; ++i) {
             if (answer.charAt(i) === submission.charAt(i)) {
@@ -69,6 +69,10 @@ class Hack extends React.Component {
 
         this.setState((prevState) => ({ value: event.target.value }))
     }
+    
+    TAsHint() {
+        alert("The correct answer is: #'cX%0");
+    }
 
     render() {
         let button;
@@ -87,6 +91,7 @@ class Hack extends React.Component {
                 </label>
                 <input type="submit" id="button" value="Submit"/>
             </form>
+            <button type="button" onClick={this.TAsHint}> Hint for the TAs</button>
             <table>
                 <tr>
                     <th>Number of correct characters in the correct space</th>
--- my-app/src/pages/Win.js
+++ my-app/src/pages/Win.js
@@ -5,6 +5,7 @@ const Win = () => {
   return (
     <div>
       <img src={winnerImg} alt="If this gif loaded, you'd be dead." />
+      <br/>
       <Link to="/Home">Return to Start Page</Link>
       
       
